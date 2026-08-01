f90d21e566208748e69d54fa0bc8581351a92fe0
kamalweldingworks <kumarisrilanka65@gmail.com>
2026-08-01 11:24:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 
+// Sri Lanka Kamal Welding Works
+
+alert("Welcome to Sri Lanka Kamal Welding Works App!");
+
+const buttons = document.querySelectorAll("button");
+
+buttons[0].addEventListener("click", function () {
+    alert("Total Calculator - Coming Soon");
+});
+
+buttons[1].addEventListener("click", function () {
+    alert("PDF Download - Coming Soon");
+});
+
+buttons[2].addEventListener("click", function () {
+    alert("WhatsApp Share - Coming Soon");
+});
