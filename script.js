673ac0cd6052c0274a40ceb7b6da55c8c394e019
kamalweldingworks <kumarisrilanka65@gmail.com>
2026-08-02 07:23:01 +0530

    Show alert with total amount in calculateTotal function
    Added alert to display total amount after calculation.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-
 function calculateTotal() {
 
-    let material = Number(document.getElementById("materialCost").value);
-    let labour = Number(document.getElementById("labourCharge").value);
+    let material = Number(document.getElementById("materialCost").value) || 0;
+    let labour = Number(document.getElementById("labourCharge").value) || 0;
 
     let total = material + labour;
 
     document.getElementById("totalAmount").value = total;
+
+    alert("Total Amount = Rs. " + total);
 
 }
