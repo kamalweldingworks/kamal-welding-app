1340a036521c88a07a49fdf622ed6cf60a069b86
kamalweldingworks <kumarisrilanka65@gmail.com>
2026-08-06 10:14:00 +0530

    Define job types and materials for welding works
    Added job types and associated materials for various projects.

--- jobtypes.js
+++ jobtypes.js
@@ -1,1 +1,66 @@
 
+/* ==========================================
+   Sri Lanka Kamal Welding Works
+   Job Types Database V1.0
+========================================== */
+
+const JOB_TYPES = {
+
+    Gate: [
+        "2x2 Box Bar",
+        "1x1 Box Bar",
+        "12mm Rod",
+        "Welding Rod",
+        "Primer Paint",
+        "Finish Paint",
+        "Hinges",
+        "Lock",
+        "Handle"
+    ],
+
+    "Sliding Gate": [
+        "2x2 Box Bar",
+        "1x1 Box Bar",
+        "12mm Rod",
+        "Welding Rod",
+        "Sliding Wheel",
+        "Sliding Track",
+        "Primer Paint",
+        "Finish Paint"
+    ],
+
+    Handrail: [
+        "2x2 Box Bar",
+        "1x1 Box Bar",
+        "Welding Rod",
+        "Grinding Disc",
+        "Primer Paint",
+        "Finish Paint"
+    ],
+
+    Roof: [
+        "2x2 Box Bar",
+        "C Purlin",
+        "Amano Sheet",
+        "Roof Screw",
+        "Welding Rod"
+    ],
+
+    Grill: [
+        "1x1 Box Bar",
+        "10mm Rod",
+        "Welding Rod",
+        "Primer Paint",
+        "Finish Paint"
+    ],
+
+    Stair: [
+        "2x2 Box Bar",
+        "1x1 Box Bar",
+        "MS Plate 5mm",
+        "Welding Rod",
+        "Primer Paint",
+        "Finish Paint"
+    ]
+
+};
